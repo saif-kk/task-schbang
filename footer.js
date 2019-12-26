7bbaeb7a713edc7657dcb3f28af7c6841253a483
saif kondkari <kondkarisaif@gmail.com>
2019-12-27 04:29:13 +0530

    UI Chnages

---
+++ footer.js
@@ -0,0 +1,30 @@
+var footer= `<footer>
+    <div class="container" style="padding:2%">
+        <div class="row">
+            <a class="navbar-brand" href="#" style="margin: auto">
+                <img src="assets/images/Group_112.png" alt="logo">
+            </a>
+        </div>
+        <div class="row">
+            <nav class="navbar navbar-expand-sm" style="width: 100%;margin: 1.5%">
+                <ul class="navbar-nav" style="margin-left: 22%">
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Our Story</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Products</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Recipe</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Flavour Stories</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</footer>`
---
+++ header.js
@@ -0,0 +1,32 @@
+var header = `<header id="topnav">
+<nav class="navbar navbar-expand-sm ">
+  <!-- Brand/logo -->
+  <a class="navbar-brand" href="#">
+    <img src="assets/images/Group_112.png" alt="logo" style="margin-left:25%">
+  </a>
+  
+  <!-- Links -->
+  <ul class="navbar-nav" style="margin-left: 22%">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Our Story</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Products</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Recipe</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Flavour Stories</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Contact Us</a>
+    </li>
+  </ul>
+
+  <button class="btn btn-find" type="submit">Find Us</button>
+  <button class="btn" style="margin-left:6%"><img src="assets/images/search.png" class="img-responsive"></button>
+</nav>
+
+
+</header>`
