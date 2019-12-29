6177fe7be9ba71086d592d97382283b78ce3ff33
saif <kondkarisaif@gmail.com>
2019-12-29 20:19:25 +0530

    all done

--- footer.js
+++ footer.js
@@ -1,10 +1,11 @@
 var footer= `<footer>
-    <div class="container" style="padding:2%">
+    <div class="container" style="padding:1%">
         <div class="row">
             <a class="navbar-brand" href="#" style="margin: auto">
                 <img src="assets/images/Group_112.png" alt="logo">
             </a>
         </div>
+
         <div class="row">
             <nav class="navbar navbar-expand-sm" style="width: 100%;margin: 1.5%">
                 <ul class="navbar-nav mr-auto" style="margin-left: 22%">
@@ -28,15 +29,29 @@ var footer= `<footer>
         </div>
 
         <div class="row">
-            <form id="subscribeForm" style="width: 50%;margin: 0 auto;text-align: center;">
+            <form id="subscribeForm">
                 <div class="form-group row">
-                    <label class="col-sm-3 col-form-label" for="searchBar">Subscribe</label>
-                    <input type="text" class="col-sm-7" id="searchBar" placeholder="ENTER YOUR EMAIL">
-                    <div class="col-sm-2">
-                        <img src="assets/images/footerR.png">
-                    </div>
+                    <label class="col-sm-2 col-form-label" for="searchBar">Subscribe</label>
+                    <input type="text" class="col-sm-6" id="searchBar" placeholder="ENTER YOUR EMAIL"> <img src="assets/images/footerR.png" style="float:left;margin-left:2%">
                 </div>
             </form>
         </div>
+
+        <div class="row" style="margin:1.3% 0">
+            <div class="footerL row">
+                <div class="col-sm-4">
+                    <a href="">Privacy</a> | <a href="">Terms & Conditions</a> | <a href="">Feedback</a> 
+                </div>
+                <div class="col-sm-4">
+                    Share Us <a href=""><img src="assets/images/whatsapp-logo.png" class="img-fluid"></a> 
+                    <a href=""><img src="assets/images/instagram.png" class="img-fluid"></a>
+                    <a href=""><img src="assets/images/facebook.png" class="img-fluid"></a>
+                </div>
+                <div class="col-sm-4">
+                    © 2019  Copyrigts
+                </div>
+            </div>
+        </div>
+
     </div>
 </footer>`
